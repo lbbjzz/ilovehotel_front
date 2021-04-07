443e6a3d21169b7bbcc0d1167b6eb8594451b5fa
lbbjzz <lbbjzz@gmail.com>
2021-04-07 22:26:23 +0800

    Fix: Style Problems

--- pages/details/intro-details.vue
+++ pages/details/intro-details.vue
@@ -6,7 +6,9 @@
     <div>
       <el-image :src="intro.image"></el-image>
     </div>
-    <div>{{ intro.content }}</div>
+    <div style="margin-top: 20px;margin-bottom: 20px">
+      <span style="line-height: 30px">{{ intro.content }}</span>
+    </div>
     <el-backtop></el-backtop>
   </div>
 </template>
